e7a9ce029cb074586bd5ea940bb67abb097b9a44
Nilansh Bansal <bansalnilansh@gmail.com>
2018-10-03 20:01:29 +0530

    added form for message creation & showed incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -8,13 +8,20 @@ socket.on('disconnect',function(){
     console.log('disconnected from server!');
 });
 
-socket.on('newMessage',function(data){
-    console.log('new Message !',data);
+socket.on('newMessage',function(message){
+    console.log('new Message !',message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text} `);
+
+    jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage',{
-    from: 'Frank',
-    text:'Hi'
-},function(data){
-    console.log(data);
+jQuery('#message-form').on('submit',function(e){
+    e.preventDefault();
+    socket.emit('createMessage',{
+        from:'User',
+        text:jQuery('[name=message]').val()
+    },function(data){
+        console.log(data);
+    });
 });
